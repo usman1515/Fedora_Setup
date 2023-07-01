b040554d091fe1a24ee72b3b0d6b1a546d69ebf4
usman1515 <usmansiddique1515@gmail.com>
2023-07-01 18:22:13 +0500

    created bash script install_cli_packages

---
+++ install_cli_packages.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo dnf install -y curl
+sudo dnf install -y wget
+
+sudo dnf install -y grep
+sudo dnf install -y sed
+sudo dnf install -y qawk
+sudo dnf install -y fzf
+
+sudo dnf install -y fortune-mod
+sudo dnf install -y cowsay
+sudo dnf install -y neofetch
+sudo dnf install -y cmatrix
+
+sudo dnf install -y tmux
+sudo dnf install -y vim-enhanced 
+sudo dnf install -y neovim
+sudo dnf install -y bat
+
+sudo dnf install -y tree
+sudo dnf install -y exa
+sudo dnf install -y ncdu
+sudo dnf install -y ImageMagick
+
+sudo dnf install -y htop
+sudo dnf install -y unzip
