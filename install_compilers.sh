50259f8eda5d427d7f5eb96faa6e416e69f91a71
usman1515 <usmansiddique1515@gmail.com>
2023-07-01 18:21:59 +0500

    created bash script install_compilers and install_docker

---
+++ install_compilers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo dnf install -y git
+sudo dnf install -y gcc gcc-c++
+sudo dnf install -y autoconf automake
+sudo dnf install -y cmake
+sudo dnf install -y python3 python3-pip
+sudo dnf install -y lua
+sudo dnf install -y java-openjdk
+sudo dnf install -y rust cargo
