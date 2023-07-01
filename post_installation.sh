9a59d872e7e31b4d62ad53ebc3f6d872d950b692
usman1515 <usmansiddique1515@gmail.com>
2023-07-01 18:20:31 +0500

    created bash script post_installation

---
+++ post_installation.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# link: https://rpmfusion.org/Configuration
+
+# enable access to both the free and the nonfree repositories
+sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# appstream metadata to enable users to install packages using Gnome/KDE
+sudo dnf groupupdate core
+
+# enable flathub
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# install media codecs
+sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin --allowerasing --skip-broken
+
+# install preload
+sudo dnf copr enable elxreno/preload -y && sudo dnf install -y preload
+
+# enable 3rd party reporsitories
+sudo dnf install -y fedora-workstation-repositories
+    
