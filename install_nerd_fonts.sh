ca54f5c661f5f3d21a13b56352d3357b96ca3477
usman1515 <usmansiddique1515@gmail.com>
2024-02-20 16:58:39 +0100

    added generic and microsoft fonts

--- install_nerd_fonts.sh
+++ install_nerd_fonts.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+# ========== install Nerd Fonts
 # create folder
 mkdir ~/.fonts
-
 # download fonts
 wget -P ~/.fonts/ https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip
-
 # goto folder and unzip
 cd ~/.fonts
 unzip CascadiaCode.zip
+
+
+# ========== install generic fonts 
+dnf search fonts
+sudo dnf install -y libreoffice-opensymbol-fonts
+
+
+# ========== install Microsoft fonts (Calibri, Times New Roman etc)
+# URL: https://mscorefonts2.sourceforge.net/
+# install pre req packages
+sudo dnf install -y curl cabextract xorg-x11-font-utils fontconfig-devel
+# install fonts
+sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+# update font cache
+sudo fc-cache -v
