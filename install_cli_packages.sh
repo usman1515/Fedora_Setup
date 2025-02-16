8110ed8263f4ec49c876611cdd1e51619a822f39
usman1515 <usmansiddique1515@gmail.com>
2025-02-16 19:29:47 +0100

    feat: added new packages

--- install_cli_packages.sh
+++ install_cli_packages.sh
@@ -9,8 +9,8 @@ sudo dnf install -y awk
 sudo dnf install -y fzf
 sudo dnf install -y ripgrep
 
-sudo dnf install -y fortune-mod
-sudo dnf install -y cowsay
+# sudo dnf install -y fortune-mod
+# sudo dnf install -y cowsay
 sudo dnf install -y fastfetch
 sudo dnf install -y cmatrix
 
@@ -18,12 +18,13 @@ sudo dnf install -y tmux
 sudo dnf install -y vim-X11
 sudo dnf install -y neovim
 sudo dnf install -y bat
-sudo dnf copr enable -y varlad/zellij 
+sudo dnf copr enable -y varlad/zellij
 sudo dnf install -y zellij
 
 sudo dnf install -y tree
 sudo dnf install -y exa
 sudo dnf install -y ncdu
+sudo dnf install -y ffmpeg
 sudo dnf install -y ImageMagick
 
 sudo dnf install -y btop
@@ -32,3 +33,4 @@ sudo dnf install -y p7zip
 sudo dnf install -y tokei
 
 sudo dnf install -y NetworkManager-tui
+sudo dnf install -y qalculate
