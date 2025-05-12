50c8bcb81d636b858abe185a63d63e1902924797
usman1515 <usmansiddique1515@gmail.com>
2025-05-12 19:09:04 +0200

    feat: added vscodium

--- install_gui_packages.sh
+++ install_gui_packages.sh
@@ -8,6 +8,11 @@ dnf check-update
 sudo dnf install -y code
 source install_vscode_extensions.sh
 
+# install vscodium
+sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h\n" | sudo tee -a /etc/yum.repos.d/vscodium.repo
+sudo dnf install codium
+
 # sudo dnf install -y texmaker
 sudo dnf install -y meld
 
