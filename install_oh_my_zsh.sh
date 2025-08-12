0f9a019bf468d6496560de0e95697b77870cec77
usman1515 <usmansiddique1515@gmail.com>
2025-08-12 23:00:41 +0200

    feat: added yellow print statements

--- install_oh_my_zsh.sh
+++ install_oh_my_zsh.sh
@@ -2,21 +2,30 @@
 
 cd ~
 
+RED='\e[31m'
+YELLOW='\e[33m'
+GREEN='\e[32m'
+NC='\e[0m'
+
+echo -e "${YELLOW} install ZSH and CHSH ${NC}"
 # install zsh
 sudo dnf install -y zsh
 # install chsh
 sudo dnf install -y util-linux
 
 # download oh my zsh
+echo -e "${YELLOW} download oh my ZSH ${NC}"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# download zsh syntax highlighting
+
+echo -e "${YELLOW} download zsh-syntax-highlighting ${NC}"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-# download zsh auto suggestions
+
+echo -e "${YELLOW} download zsh-autosuggestions ${NC}"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-# download zsh auto switch virtualenv
+
+echo -e "${YELLOW} download zsh-autoswitch-virtualenv ${NC}"
 git clone "https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git" "$ZSH_CUSTOM/plugins/autoswitch_virtualenv"
 
-# download powerlevel 10k theme
+echo -e "${YELLOW} download powerlevel 10k ${NC}"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
 
--- post_installation.sh
+++ post_installation.sh
@@ -2,24 +2,29 @@
 
 # link: https://rpmfusion.org/Configuration
 
-# enable access to both the free and the nonfree repositories
+RED='\e[31m'
+YELLOW='\e[33m'
+GREEN='\e[32m'
+NC='\e[0m'
+
+echo -e "${YELLOW} enable access to both the free and the nonfree repositories ${NC}"
 sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-# explicitly enable the repository
+echo -e "${YELLOW} explicitly enable the repository ${NC}"
 sudo dnf config-manager --enable fedora-cisco-openh264
 
-# appstream metadata to enable users to install packages using Gnome/KDE
+echo -e "${YELLOW} update all packagges in core group ${NC}"
 sudo dnf groupupdate core
 
-# enable flathub
+echo -e "${YELLOW} enable flathub ${NC}"
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-# install media codecs
+echo -e "${YELLOW} install media codecs ${NC}"
 sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin --allowerasing --skip-broken
 
-# install preload
+echo -e "${YELLOW} install preload (daemon that preloads frequently used apps) ${NC}"
 sudo dnf copr enable elxreno/preload -y && sudo dnf install -y preload
 
-# enable 3rd party reporsitories
+echo -e "${YELLOW} enable 3rd party reporsitories ${NC}"
 sudo dnf install -y fedora-workstation-repositories
-    
+
