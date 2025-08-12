6464d12d70c00e0a0b2ae2d3b77d3ce89386b74c
usman1515 <usmansiddique1515@gmail.com>
2025-08-12 23:00:26 +0200

    feat: using nerd-font repo rather than downloading zip fonts

--- install_nerd_fonts.sh
+++ install_nerd_fonts.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-# ========== install Nerd Fonts
-# create folder
-mkdir ~/.fonts
-# download fonts
-wget -P ~/.fonts/ https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip
-# goto folder and unzip
-cd ~/.fonts
-unzip CascadiaCode.zip
+RED='\e[31m'
+YELLOW='\e[33m'
+GREEN='\e[32m'
+NC='\e[0m'
 
+echo -e "${YELLOW} clone and install nerd fonts ${NC}"
+git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git ~/nerd_fonts
+cd ~/nerd_fonts
 
-# ========== install generic fonts 
+./install.sh FiraCode
+./install.sh FiraMono
+./install.sh CascadiaCode
+./install.sh CascadiaMono
+
+echo -e "${YELLOW} install generic fonts ${NC}"
 dnf search fonts
 sudo dnf install -y libreoffice-opensymbol-fonts
 
-
-# ========== install Microsoft fonts (Calibri, Times New Roman etc)
+echo -e "${YELLOW} install Microsoft fonts (Calibri, Times New Roman etc) ${NC}"
 # URL: https://mscorefonts2.sourceforge.net/
 # install pre req packages
 sudo dnf install -y curl cabextract xorg-x11-font-utils fontconfig-devel
@@ -23,3 +26,4 @@ sudo dnf install -y curl cabextract xorg-x11-font-utils fontconfig-devel
 sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
 # update font cache
 sudo fc-cache -v
+
